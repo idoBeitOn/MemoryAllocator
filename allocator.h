50b923622c1c11d944a0580c5a1afd1fd678bcc2
Ido Beit On <130286681+idoBeitOn@users.noreply.github.com>
2026-07-07 19:13:08 +0300

    Add Allocator struct

--- allocator.h
+++ allocator.h
@@ -1,4 +1,15 @@
 #include <stddef.h>
+#include "Stats.h"
+#include "BlockHeader.h"
+
+typedef struct Allocator
+{
+    uint8_t* heapStart;
+    BlockHeader* firstBlock;
+    AllocatorStats stats;
+} Allocator;
+
+
 
 void* my_malloc(size_t size);
 void my_free(void* ptr);
