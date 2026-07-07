b7dad020287d80313fd4482bd6efba4b7525d0e1
Ido Beit On <130286681+idoBeitOn@users.noreply.github.com>
2026-07-07 13:50:15 +0300

    Add BlockHeader struct, .gitignore, and main.c
    Created BlockHeader.h which is defining a BlockHeader struct to store memory block metadata files.

---
+++ BlockHeader.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+/*
+Defines metadata for one block of memory.
+Every block begins with a header.
+The user receives a pointer to the memory after the header.
+*/
+typedef struct BlockHeader
+{
+    uint8_t marker; 
+    uint32_t length;//Stores the size of the user's memory
+    bool inUse;
+    struct BlockHeader* prev;
+    struct BlockHeader* next;
+} BlockHeader;
